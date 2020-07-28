4d2b2c4c7b11e57de37165d4a1f6f515e13834df
Diab Neiroukh <lazerl0rd@thezest.dev>
2020-07-28 16:27:56 +0100

    prebuiltapks: Update priv-app permissions for microG
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>

--- GsfProxy/Android.mk
+++ GsfProxy/Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-com.google.android.gsf.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := permissions/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := whitelist-com.google.android.gsf.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
@@ -16,5 +24,5 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := whitelist-com.google.android.gsf.xml
+LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gsf.xml whitelist-com.google.android.gsf.xml
 include $(BUILD_PREBUILT)
